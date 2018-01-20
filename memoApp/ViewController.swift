2c11090c7923139399f01275530212114d5a2c17
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-21 00:24:58 +0900

    first commit

---
+++ memoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memoApp
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/01/20.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

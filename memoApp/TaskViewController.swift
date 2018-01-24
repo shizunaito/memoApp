c8b25ec1ebf0707895191b1123b6a11c96ac5dab
shizunaito <shizuna.itoh@gmail.com>
2018-01-24 22:00:05 +0900

    add task vc

---
+++ memoApp/TaskViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TaskViewController.swift
+//  memoApp
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/01/24.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

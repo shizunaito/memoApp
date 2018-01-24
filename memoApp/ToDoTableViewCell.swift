b67cda47b51adfa5106caf91fac6d7a736c8bbc4
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-24 21:12:32 +0900

    create ToDoTable

---
+++ memoApp/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  memoApp
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/01/24.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

3f2651f11aaa4701d29f06b971a2b75f2b86e4ef
shizunaito <shizuna.itoh@gmail.com>
2018-01-30 20:57:24 +0900

    add done table

---
+++ memoApp/Done/DoneTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DoneTableViewCell.swift
+//  memoApp
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/01/30.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+import UIKit
+
+class DoneTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

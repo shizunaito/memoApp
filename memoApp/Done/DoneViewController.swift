c6ef6481dcbb3007a4e7135abfd9fd640b685b27
shizunaito <shizuna.itoh@gmail.com>
2018-11-23 22:26:53 +0900

    DoneVCの画面のデザインを修正

---
+++ memoApp/Done/DoneViewController.swift
@@ -0,0 +1,111 @@
+//
+//  DoneTableViewController.swift
+//  memoApp
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/01/30.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import RxSwift
+import RxCocoa
+
+class DoneViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    private var tasks: Results<Task>?{
+        do{
+            let realm = try Realm()
+            return realm.objects(Task.self).filter("status = 1")
+        } catch {
+        }
+        return nil
+    }
+
+    private var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "DoneTableViewCell", bundle: nil), forCellReuseIdentifier: "doneTableViewCell")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension DoneViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tasks?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "doneTableViewCell", for: indexPath) as? DoneTableViewCell, let tasks = tasks else {
+            return UITableViewCell()
+        }
+
+        cell.task = tasks[indexPath.row]
+        cell.titleLabel.text = tasks[indexPath.row].title
+
+        cell.checkButton.rx.tap
+            .subscribe { _ in
+                tableView.reloadData()
+            }
+            .disposed(by: disposeBag)
+
+        return cell
+    }
+
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     }
+     */
+
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+}
